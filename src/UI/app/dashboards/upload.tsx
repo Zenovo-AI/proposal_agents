b421d0784d2421306c5601199a98dd722e1a946a
CertifiedAuthur <donrozzay1@gmail.com>
2025-06-23 04:34:37 +0100

    backend now runs on https

--- src/UI/app/dashboards/upload.tsx
+++ src/UI/app/dashboards/upload.tsx
@@ -121,7 +121,7 @@ const UploadPanel = ({ onBack }: UploadPanelProps) => {
     setUploadSuccess(false)
 
     try {
-      const res = await fetch("http://161.35.119.194:8000/api/ingress-file", {
+      const res = await fetch("https://147.182.178.16/api/ingress-file", {
         method: "POST",
         body: formData,
         credentials: "include",
--- src/test.py
+++ src/test.py
@@ -1,21 +1,38 @@
-from database.db_helper import save_metadata_to_db, insert_document  # <-- make sure insert_document exists
+# from database.db_helper import save_metadata_to_db, insert_document  # <-- make sure insert_document exists
 
-# First insert the document entry
-insert_document(
-    doc_id="test_doc_minimal",
-    file_name="minimal.pdf",
-    file_content="Minimal document content here"
-)
+# # First insert the document entry
+# insert_document(
+#     doc_id="test_doc_minimal",
+#     file_name="minimal.pdf",
+#     file_content="Minimal document content here"
+# )
 
-# Now insert the metadata
-data = {
-    "id": "test_doc_minimal",
-    "organization_name": "Minimal Org",
-    "rfq_number": "000",
-    "rfq_title": "Minimal RFQ",
-    "submission_deadline": None,
-    "country_or_region": "Nowhere",
-    "filename": "minimal.pdf"
-}
+# # Now insert the metadata
+# data = {
+#     "id": "test_doc_minimal",
+#     "organization_name": "Minimal Org",
+#     "rfq_number": "000",
+#     "rfq_title": "Minimal RFQ",
+#     "submission_deadline": None,
+#     "country_or_region": "Nowhere",
+#     "filename": "minimal.pdf"
+# }
 
-save_metadata_to_db(data)
+
+# save_metadata_to_db(data)
+
+def cleanText(text, censorList):
+    censor_text_list = []
+    for word in text.split():
+        if word.lower().strip('!?.') in censorList:
+            censor_text_list.append("****")
+        else:
+            censor_text_list.append(word)
+    return " ".join(censor_text_list)
+
+# theText = ['A', 'baby', 'donkey', 'is', 'called', 'an', 'ass', 'not', 'an', 'idiot']
+theText = "A baby donkey is called an ass not an idiot!"
+theCensorList = ["ass", "idiot"]
+
+censored_text = cleanText(theText, theCensorList)
+print(censored_text)
